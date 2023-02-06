3d4275d7d8b9d4c7c107620b0e9be820e4318cdd
Jack Sleight <jacksleight@gmail.com>
2023-02-06 13:49:05 +0000

    Style menu (#25)
    * WIP
    
    * WIP
    
    * WIP

--- resources/js/addon.js
+++ resources/js/addon.js
@@ -3,6 +3,7 @@ import '../css/addon.scss';
 import Span from './marks/span'
 import Div from './nodes/Div'
 import Core from './extensions/core'
+import MenuButton from "./components/MenuButton.vue";
 import { styleToIcon } from './icons';
 
 const types = {
@@ -55,11 +56,20 @@ Statamic.booting(() => {
 
     // Initialization
 
-    const { pro, store, attr, styles, styleTypes } = Statamic.$config.get('bard-texstyle');
+    const btsConfig = Statamic.$config.get('bard-texstyle');
+    const { pro, store, attr, styles, styleTypes } = btsConfig;
 
     // Extensions
 
     Statamic.$bard.addExtension(() => Core.configure({ attr, styleTypes }));
+    Statamic.$bard.addExtension(({ bard }) => {
+        bard.buttons.forEach(button => {
+            if (bard.config.bts_menu.includes(button.name)) {
+                button.visibleWhenActive = 'btsVoid';
+            }
+        });
+        return null;
+    });
     Statamic.$bard.addExtension(() => Span);
     if (pro) {
         Statamic.$bard.addExtension(() => Div);
@@ -84,10 +94,20 @@ Statamic.booting(() => {
                 activeName: type.ext,
                 html: icon,
                 command: (editor, args) => editor.chain().focus()[type.cmd](args).run(),
+                btsStyle: style,
             };
             buttons.splice(buttons.indexOf(key), 0, button(data));
         });
     });
+    // Statamic.$bard.buttons((buttons, button) => {
+    //     buttons.splice(buttons.indexOf('bts_menu'), 0, button({
+    //         name: 'bts_menu',
+    //         text: 'Style',
+    //         component: MenuButton,
+    //         html: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2"><path fill="currentColor" d="M12 17.989V16h.532a.67.67 0 0 0 .633-.887L12.349 13H6.117l-.815 2.113a.67.67 0 0 0 .633.887H7v2H0v-2h.43a1.4 1.4 0 0 0 1.32-.935L7.257 0h3.952l4.768 13.042-.346.319c-1.407 1.317-1.454 1.899-1.909 4.069a.865.865 0 0 0-.016.258 1.074 1.074 0 0 0-.114-.006c-.648.007-1.166.12-1.592.307ZM7.114 10h4.238L9.093 3.801 7.114 10Z" style="fill-rule:nonzero"/><path fill="currentColor" d="M0 21.398c5.504.456 3.533-5.392 8.626-5.445l2.206 1.841c.549 6.645-7.579 8.127-10.832 3.604Zm16.878-8.538c1.713-2.687 7.016-11.698 7.016-11.698.423-.747-.515-1.528-1.17-.976 0 0-7.887 6.857-10.213 9.03-1.838 1.719-1.846 2.504-2.441 5.336l2.016 1.681c2.67-1.098 3.439-1.248 4.792-3.373Z" style="fill-rule:nonzero" transform="translate(7.766 7.766) scale(.6764)"/></svg>`,
+    //         btsConfig,
+    //     }));
+    // });
 
     // CSS
 
@@ -116,6 +136,7 @@ Statamic.booting(() => {
             : `${type.tag}`;
         const selector = `.bard-fieldtype .ProseMirror ${tag}[data-bts="${style[store]}"]`;
         css.push(`.bard-fieldtype .ProseMirror ${tag}[data-bts="${style[store]}"] { ${style.cp_css} }`);
+        css.push(`.bard-fieldtype .bts-menu-preview[data-bts-match~="${key}"] { ${style.cp_css} }`);
         css.push(typeof style.cp_css === 'object'
             ? objectToCss(selector, style.cp_css)
             : `${selector} { ${style.cp_css} }`);   
@@ -126,5 +147,19 @@ Statamic.booting(() => {
     const el = document.createElement('style');
     el.appendChild(document.createTextNode(css.join(' ')));
     document.head.appendChild(el);
+
+    Vue.directive('bts-click-outside', {
+        bind: function (el, binding, vnode) {
+            el.clickOutsideEvent = function (event) {
+                if (!(el == event.target || el.contains(event.target))) {
+                    vnode.context[binding.expression](event);
+                }
+            };
+            document.body.addEventListener('click', el.clickOutsideEvent)
+        },
+        unbind: function (el) {
+            document.body.removeEventListener('click', el.clickOutsideEvent)
+        },
+    });
 
 });
---
+++ resources/js/components/MenuDropdown.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <div class="bts-menu-dropdown">
+        <div class="bts-menu-items">
+            <MenuItem
+                v-for="item in items"
+                v-bind:key="item.name"
+                :item="item"
+                :config="config"
+                :bard="bard"
+                :editor="editor"
+                :btsConfig="btsConfig"
+                @bts-menu-click="$emit('bts-menu-click')"
+            />
+        </div>
+        
+    </div>
+
+</template>
+
+<script>
+import MenuItem from './MenuItem.vue';
+
+export default {
+
+    components: {
+        MenuItem,
+    },
+
+    props: {
+        config: {},
+        bard: {},
+        editor: {},
+        btsConfig: {},
+    },
+
+    computed: {
+
+        items() {
+            const buttons = this.bard.buttons;
+            const menu = this.config.bts_menu;
+            return buttons.filter(button => {
+                return typeof button === 'object' && menu.includes(button.name);
+            });    
+        },
+
+    },
+
+}
+</script>
