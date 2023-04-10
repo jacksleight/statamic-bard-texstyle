81c073e1d3936795b74e211cccbba6294331b582
Jack Sleight <jacksleight@gmail.com>
2023-04-10 18:03:45 +0100

    WIP

---
+++ src/OptionManager.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace JackSleight\StatamicBardTexstyle;
+
+use Statamic\Statamic;
+use Statamic\Support\Arr;
+use Statamic\Support\Str;
+
+class OptionManager
+{
+    protected $types = [
+        'heading' => [
+            'pro' => false,
+        ],
+        'paragraph' => [
+            'pro' => false,
+        ],
+        'btsSpan' => [
+            'pro' => false,
+        ],
+        'link' => [
+            'pro' => false,
+        ],
+        'bulletList' => [
+            'pro' => false,
+        ],
+        'orderedList' => [
+            'pro' => false,
+        ],
+        'btsDiv' => [
+            'pro' => true,
+        ],
+    ];
+
+    protected $attributeTypes = [
+        'blockquote' => [],
+        'bold' => [],
+        'bulletList' => [],
+        'code' => [],
+        'codeBlock' => ['language'],
+        'heading' => ['level'],
+        'horizontalRule' => [],
+        'image' => ['src', 'alt', 'title'],
+        'italic' => [],
+        'link' => ['href', 'rel', 'target', 'title'],
+        'listItem' => [],
+        'orderedList' => [/*'start'*/],
+        'paragraph' => [],
+        'strike' => [],
+        'subscript' => [],
+        'superscript' => [],
+        'table' => [],
+        'tableCell' => ['rowspan', 'colspan', 'colwidth'],
+        'tableHeader' => ['rowspan', 'colspan', 'colwidth'],
+        'tableRow' => [],
+        'underline' => [],
+    ];
+
+    protected $styleTypes = [
+        'btsSpan',
+        'bulletList',
+        'heading',
+        'link',
+        'orderedList',
+        'paragraph',
+    ];
+
+    protected $aliases = [
+        'span' => 'btsSpan',
+        'div' => 'btsDiv',
+    ];
+
+    protected $config;
+
+    protected $pro;
+
+    public function __construct($config, $pro)
+    {
+        $this->config = $config;
+        $this->pro = $pro;
+    }
+
+    public function resolve()
+    {
+        $store = data_get($this->config, 'store', 'class');
+        $attr = $store === 'class' ? 'class' : 'bts_key';
+
+        $defaultClasses = $this->resolveDefaultClasses();
+        [$styles, $types] = $this->resolveStylesAndTypes();
+        $styleTypes = $this->resolveStyleTypes($styles, $types);
+        $classTypes = $this->resolveClassTypes($styleTypes, $defaultClasses);
+
+        $attributes = $this->resolveAttributes($classTypes);
+        $attributeTypes = $this->resolveAttributeTypes($attributes);
+
+        $styleOptions = $this->resolveStyleOptions($styles);
+
+        $version = Statamic::version();
+        $major = $version !== 'dev-master'
+            ? (int) Str::before($version, '.')
+            : 100;
+
+        return [
+            'major' => $major,
+            'pro' => $this->pro,
+            'store' => $store,
+            'attr' => $attr,
+            'styles' => $styles,
+            'types' => $types,
+            'attributes' => $attributes,
+            'styleTypes' => $styleTypes,
+            'classTypes' => $classTypes,
+            'attributeTypes' => $attributeTypes,
+            'defaultClasses' => $defaultClasses,
+            'styleOptions' => $styleOptions,
+        ];
+    }
+
+    protected function resolveStylesAndTypes()
+    {
+        $styles = data_get($this->config, 'styles', []);
+        $styles = $this->normalizeStyles($styles);
+
+        $types = collect($this->types)
+            ->map(fn ($type, $key) => array_merge($type, ['key' => $key]))
+            ->filter(fn ($type) => ! $type['pro'] || $this->pro)
+            ->all();
+
+        $usedTypes = [];
+        $styles = collect($styles)
+            ->map(fn ($style, $key) => array_merge($style, ['key' => $key]))
+            ->map(fn ($style) => array_merge($style, ['type' => $this->aliases[$style['type']] ?? $style['type']]))
+            ->filter(fn ($style) => isset($types[$style['type']]))
+            ->each(function ($style) use (&$usedTypes) {
+                $usedTypes[$style['type']] = true;
+            })
+            ->all();
+
+        $types = collect($types)
+            ->filter(fn ($type, $key) => isset($usedTypes[$key]))
+            ->all();
+
+        return [$styles, $types];
+    }
+
+    protected function resolveAttributes($classTypes)
+    {
+        if (! $this->pro) {
+            return [];
+        }
+
+        $attributes = data_get($this->config, 'attributes', []);
+
+        $attributes = collect($attributes)
+            ->mapWithKeys(fn ($attrs, $type) => [($this->aliases[$type] ?? $type) => $attrs])
+            ->filter(fn ($attrs, $type) => array_key_exists($type, $this->attributeTypes))
+            ->map(function ($attrs, $type) use ($classTypes) {
+                return collect($attrs)->map(function ($attr, $name) use ($type, $classTypes) {
+                    return array_merge($attr, [
+                        'extra' => ! in_array($name, $this->attributeTypes[$type] ?? []) &&
+                            ($name !== 'class' || ! in_array($type, $classTypes)),
+                    ]);
+                })->all();
+            })
+            ->all();
+
+        return $attributes;
+    }
+
+    protected function resolveDefaultClasses()
+    {
+        $defaultClasses = data_get($this->config, 'default_classes', []);
+        $defaultClasses = $this->normalizeDefaults($defaultClasses);
+
+        return $defaultClasses;
+    }
+
+    protected function resolveStyleTypes($styles, $types)
+    {
+        return collect($styles)
+            ->pluck('type')
+            ->unique()
+            ->values()
+            ->all();
+    }
+
+    protected function resolveClassTypes($styleTypes, $defaultClasses)
+    {
+        return collect($styleTypes)
+            ->merge(collect($defaultClasses)->flatMap(fn ($value) => $value)->keys())
+            ->unique()
+            ->values()
+            ->all();
+    }
+
+    protected function resolveAttributeTypes($attributes)
+    {
+        return collect($attributes)
+            ->keys()
+            ->all();
+    }
+
+    protected function resolveStyleOptions($styles)
+    {
+        if (! $this->pro) {
+            return [];
+        }
+
+        return collect($styles)
+            ->filter(fn ($style) => in_array($style['type'], $this->styleTypes))
+            ->mapWithKeys(fn ($style, $key) => [$key => $style['name']])
+            ->merge([
+                'h1' => 'Heading 1',
+                'h2' => 'Heading 2',
+                'h3' => 'Heading 3',
+                'h4' => 'Heading 4',
+                'h5' => 'Heading 5',
+                'h6' => 'Heading 6',
+                'unorderedlist' => 'Unordered List',
+                'orderedlist' => 'Ordered List',
+            ])
+            ->sort()
+            ->all();
+    }
+
+    protected function normalizeDefaults($defaults)
+    {
+        if (array_key_exists('standard', $defaults)) {
+            return $defaults;
+        }
+
+        return [
+            'standard' => $defaults,
+        ];
+    }
+
+    /**
+     * @deprecated
+     */
+    protected function normalizeStyles($styles)
+    {
+        if (Arr::isAssoc($styles)) {
+            return $styles;
+        }
+
+        $normal = [];
+
+        foreach ($styles as $style) {
+            $key = isset($style['button'])
+                ? $style['button']
+                : ('bts_'.preg_replace('/[^\w-]/i', '_', $style['class']));
+            $style['type'] = 'paragraph';
+            $normal[$key] = $style;
+        }
+
+        return $normal;
+    }
+}
---
+++ tests/ConfigTest.php
@@ -0,0 +1,267 @@
+<?php
+
+use JackSleight\StatamicBardTexstyle\OptionManager;
+
+uses(Tests\TestCase::class);
+
+it('resolves free edition config', function () {
+    $options = (new OptionManager([
+        'styles' => [
+            'title' => [
+                'type' => 'heading',
+                'level' => 1,
+                'name' => 'Title',
+                'ident' => 'T',
+                'icon' => null,
+                'class' => 'title',
+                'cp_css' => 'font-size: 1.5em',
+                'cp_badge' => false,
+            ],
+            'twocolumns' => [
+                'type' => 'div',
+                'name' => 'Two Columns',
+                'ident' => '❙ ❙',
+                'icon' => null,
+                'class' => 'two-columns',
+                'cp_css' => 'column-count: 2; column-gap: 16px',
+                'cp_badge' => true,
+            ],
+        ],
+        'default_classes' => [
+            'standard' => [
+                'heading' => [
+                    1 => 'heading-1',
+                ],
+                'paragraph' => 'paragraph',
+            ],
+        ],
+        'attributes' => [
+            'heading' => [
+                'id' => [
+                    'type' => 'text',
+                    'display' => 'ID',
+                    'default' => null,
+                    'rendered' => true,
+                ],
+            ],
+        ],
+        'store' => 'class',
+    ], false))->resolve();
+
+    expect($options)->toEqual([
+        'major' => 3,
+        'pro' => false,
+        'store' => 'class',
+        'attr' => 'class',
+        'styles' => [
+            'title' => [
+                'type' => 'heading',
+                'level' => 1,
+                'name' => 'Title',
+                'ident' => 'T',
+                'icon' => null,
+                'class' => 'title',
+                'cp_css' => 'font-size: 1.5em',
+                'cp_badge' => false,
+                'key' => 'title',
+            ],
+        ],
+        'types' => [
+            'heading' => [
+                'pro' => false,
+                'key' => 'heading',
+            ],
+        ],
+        'attributes' => [],
+        'styleTypes' => [
+            'heading',
+        ],
+        'classTypes' => [
+            'heading',
+            'paragraph',
+        ],
+        'attributeTypes' => [],
+        'defaultClasses' => [
+            'standard' => [
+                'heading' => [
+                    1 => 'heading-1',
+                ],
+                'paragraph' => 'paragraph',
+            ],
+        ],
+        'styleOptions' => [],
+    ]);
+});
+
+it('resolves pro edition config', function () {
+    $options = (new OptionManager([
+        'styles' => [
+            'title' => [
+                'type' => 'heading',
+                'level' => 1,
+                'name' => 'Title',
+                'ident' => 'T',
+                'icon' => null,
+                'class' => 'title',
+                'cp_css' => 'font-size: 1.5em',
+                'cp_badge' => false,
+            ],
+            'twocolumns' => [
+                'type' => 'div',
+                'name' => 'Two Columns',
+                'ident' => '❙ ❙',
+                'icon' => null,
+                'class' => 'two-columns',
+                'cp_css' => 'column-count: 2; column-gap: 16px',
+                'cp_badge' => true,
+            ],
+        ],
+        'default_classes' => [
+            'standard' => [
+                'heading' => [
+                    1 => 'heading-1',
+                ],
+                'paragraph' => 'paragraph',
+            ],
+        ],
+        'attributes' => [
+            'heading' => [
+                'id' => [
+                    'type' => 'text',
+                    'display' => 'ID',
+                    'default' => null,
+                    'rendered' => true,
+                ],
+            ],
+        ],
+        'store' => 'class',
+    ], true))->resolve();
+
+    expect($options)->toEqual([
+        'major' => 3,
+        'pro' => true,
+        'store' => 'class',
+        'attr' => 'class',
+        'styles' => [
+            'title' => [
+                'type' => 'heading',
+                'level' => 1,
+                'name' => 'Title',
+                'ident' => 'T',
+                'icon' => null,
+                'class' => 'title',
+                'cp_css' => 'font-size: 1.5em',
+                'cp_badge' => false,
+                'key' => 'title',
+            ],
+            'twocolumns' => [
+                'type' => 'btsDiv',
+                'name' => 'Two Columns',
+                'ident' => '❙ ❙',
+                'icon' => null,
+                'class' => 'two-columns',
+                'cp_css' => 'column-count: 2; column-gap: 16px',
+                'cp_badge' => true,
+                'key' => 'twocolumns',
+            ],
+        ],
+        'types' => [
+            'heading' => [
+                'pro' => false,
+                'key' => 'heading',
+            ],
+            'btsDiv' => [
+                'pro' => true,
+                'key' => 'btsDiv',
+            ],
+        ],
+        'attributes' => [
+            'heading' => [
+                'id' => [
+                    'type' => 'text',
+                    'display' => 'ID',
+                    'default' => null,
+                    'rendered' => true,
+                    'extra' => true,
+                ],
+            ],
+        ],
+        'styleTypes' => [
+            'heading',
+            'btsDiv',
+        ],
+        'classTypes' => [
+            'heading',
+            'btsDiv',
+            'paragraph',
+        ],
+        'attributeTypes' => [
+            'heading',
+        ],
+        'defaultClasses' => [
+            'standard' => [
+                'heading' => [
+                    1 => 'heading-1',
+                ],
+                'paragraph' => 'paragraph',
+            ],
+        ],
+        'styleOptions' => [
+            'h1' => 'Heading 1',
+            'h2' => 'Heading 2',
+            'h3' => 'Heading 3',
+            'h4' => 'Heading 4',
+            'h5' => 'Heading 5',
+            'h6' => 'Heading 6',
+            'orderedlist' => 'Ordered List',
+            'title' => 'Title',
+            'unorderedlist' => 'Unordered List',
+        ],
+    ]);
+});
+
+it('resolves legacy paragraph style config', function () {
+    $options = (new OptionManager([
+        'styles' => [
+            [
+                'name' => 'Introduction',
+                'ident' => 'I',
+                'class' => 'intro',
+                'button' => 'introduction',
+                'cp_css' => 'font-size: 1.25em',
+            ],
+        ],
+    ], true))->resolve();
+
+    expect($options['styles'])->toEqual([
+        'introduction' => [
+            'name' => 'Introduction',
+            'ident' => 'I',
+            'class' => 'intro',
+            'button' => 'introduction',
+            'cp_css' => 'font-size: 1.25em',
+            'type' => 'paragraph',
+            'key' => 'introduction',
+        ],
+    ]);
+});
+
+it('resolves legacy default_classes config', function () {
+    $options = (new OptionManager([
+        'default_classes' => [
+            'heading' => [
+                1 => 'heading-1',
+            ],
+            'paragraph' => 'paragraph',
+        ],
+    ], true))->resolve();
+
+    expect($options['defaultClasses'])->toEqual([
+        'standard' => [
+            'heading' => [
+                1 => 'heading-1',
+            ],
+            'paragraph' => 'paragraph',
+        ],
+    ]);
+});
